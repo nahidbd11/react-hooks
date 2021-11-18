117c7640bcd2c5ea50419ce9d77652b46dd8458d
nahid <diegonahid@gmail.com>
2021-11-18 11:34:15 +0600

    working with use state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const handleCounter = () => {
+    setCounter(prevCounter => {
+      return prevCounter + 1;
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-primary">
+      <p className="lead text-light">hellow world {counter}</p>
+      <button className="btn btn-warning" onClick={handleCounter}>Count</button>
     </div>
   );
 }
